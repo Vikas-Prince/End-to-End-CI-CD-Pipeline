838ac7cbddf883795375464cbb209c31d5b0aaf6
Vikas-Prince <vikasprince30809@gmail.com>
2024-09-23 00:47:08 +0530

    updated

--- deploy/terraformscripts/resources.tf
+++ deploy/terraformscripts/resources.tf
@@ -100,7 +100,8 @@ resource "aws_instance" "ec2"{
 }
 
 # Creating Dynamic Inventory File for Ansible
-resource "null_resource" "ansibleInventory"{
+# Creating Dynamic Inventory File for Ansible
+resource "null_resource" "ansibleInventory" {
   triggers = {
     myTriggers = timestamp()
   }
@@ -110,12 +111,12 @@ resource "null_resource" "ansibleInventory"{
       echo "[k8s Master]" > inventory
       echo "${aws_instance.ec2[0].public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=${key_path}" >> inventory
       echo "[k8s slave]" >> inventory
-      for ip in ${join(" ", aws_instance.ec2[*].public_ip)}; do
-        if [ "$ip" != "${aws_instance.web.0.public_ip}" ]; then
-          echo "$ip ansible_user=ec2-user ansible_ssh_private_key_file=${key_path}" >> inventory
-        fi
-      done
+      # Remove the following loop if there are no 'web' instances
+      # for ip in ${join(" ", aws_instance.ec2[*].public_ip)}; do
+      #   if [ "$ip" != "${aws_instance.ec2[0].public_ip}" ]; then
+      #     echo "$ip ansible_user=ec2-user ansible_ssh_private_key_file=${key_path}" >> inventory
+      #   fi
+      # done
     EOT
   }
-
 }
