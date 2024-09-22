eee925da61c2113d02130bfecf27f9171342de69
Vikas-Prince <vikasprince30809@gmail.com>
2024-09-23 00:39:03 +0530

    Updated CD jenkinsfile

--- deploy/terraformscripts/resources.tf
+++ deploy/terraformscripts/resources.tf
@@ -89,7 +89,7 @@ resource "aws_instance" "ec2"{
     ami = data.aws_ami.latest-ami
     instance_type = var.instance_type
     subnet_id = aws_subnet.vpcSubnet.id
-    vpc_security_group_ids = [data.aws_Security_group.terraSecuritygp.id]
+    vpc_security_group_ids = [aws_Security_group.terraSecuritygp.id]
     key_name = data.aws_key_pair.my_key
     count = 1
 
