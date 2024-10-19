dad55773ba9551ec20d08dcbc213730f09e4eb7f
Vikas-Prince <vikasprince30809@gmail.com>
2024-10-19 22:13:49 +0530

    Updated Source Code

--- test.js
+++
@@ -1,8 +0,0 @@
-const request = require("supertest");
-const app = require("./index");
-
-describe("Server", function () {
-  it("should load the homepage successfully", function (done) {
-    request(app).get("/").expect("Content-Type", /html/).expect(200, done);
-  });
-});
